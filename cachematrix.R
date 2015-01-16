2684c147ce21ed4c54b9017d8fdb5a0e4e0c0b7a
Steve <steve.gilbert@mccombs.utexas.edu>
2015-01-16 16:33:57 -0600

    I still have no fucking idea about this assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## I really have no fucking idea how to approach this assignment.
+## The lectures are very superficial compared to the assignment.
+## Even with the explanations in the discussion forum, I am afraid 
+## that I still have no clue how the example functions work.
+## 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<-null
 }
-
-
-## Write a short comment describing this function
-
+## The following returns the inverse of the matrix, but it first
+## checks to see if it has already been calculated.  If so, it 
+## gets the results from the cache and skips the computation. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
     solve(x)
 }
